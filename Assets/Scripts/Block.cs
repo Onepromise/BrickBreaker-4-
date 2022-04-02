3fd2b21d8af546806572c66f2acdd5438ce4a84a
OnePromise <onepromise@gmail.com>
2022-04-02 12:20:28 -0400

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    [SerializeField] AudioClip breaksound;
+
+    //Cached Reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakablocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breaksound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
+
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    //For Debugging purposes
+    [SerializeField] int breakableBlocks;
+
+    //cached references
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakablocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
